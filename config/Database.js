348d1e3401ae3d7ef0c9099ccbb155c18a15eb7a
afharyo <afharyodiwseno@gmail.com>
2025-03-20 12:45:31 +0700

    edit package

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
 // Nyambungin db ke BE
-const db = new Sequelize("note-app", "root", "", {
-  host: 'localhost',
+const db = new Sequelize("note-app", "root", "4zh4r_f1k", {
+  host: '34.34.218.102',
   port: 3306,
   dialect: 'mysql'
 });
